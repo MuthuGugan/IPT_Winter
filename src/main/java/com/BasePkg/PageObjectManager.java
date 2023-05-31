3e0a9e161175032287048bcfe0d93653199ddb4b
MuthuGugan <muthukumarcm003@gmail.com>
2023-05-31 12:38:04 +0530

    Trial1

---
+++ src/main/java/com/BasePkg/PageObjectManager.java
@@ -0,0 +1,25 @@
+package com.BasePkg;
+
+import com.PageObjectModel.LoginPage;
+import com.PageObjectModel.MyProfilePage;
+
+public class PageObjectManager
+{
+
+	public LoginPage lp;
+	public MyProfilePage mp;
+
+	public MyProfilePage getMp() 
+	{
+		mp = new MyProfilePage();
+		return mp;
+	}
+
+	public LoginPage getLp() 
+	{
+		lp = new LoginPage();
+		return lp;
+	}
+	
+	
+}
